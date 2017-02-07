cff2838625f53152f48a393592d0c442f0285660
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-07 15:59:21 +0800

    Add functionality to edit and update blog posts.

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -6,19 +6,46 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  def create
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "You've published your post successfully."
+    else
+      render 'new'
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy(post_params)
+      redirect_to posts_path
+      flash[:notice] = "You've deleted your post successfully."
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+      flash[:notice] = "You've updated your post successfully."
+    else
+      render 'new'
+    end
   end
+private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
