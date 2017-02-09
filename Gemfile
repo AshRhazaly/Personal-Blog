a91fe29cdc8401ed3cc48f1ee0e319637bd9380d
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-09 22:47:57 +0800

    Add email format validation.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem 'validates_email_format_of'
 gem 'kaminari'
 gem 'client_side_validations'
 gem 'bootstrap-sass', '~> 3.3.6'
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, email_format: { message: "doesn't look like an email address" },
+                    :presence => true
   validates :content, presence: true
 end
