5494383bee8f7ef19fcc47edd61680fda790f1c3
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-09 17:08:46 +0800

    Add email address to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209070550) do
+ActiveRecord::Schema.define(version: 20170209090749) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170209070550) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
+    t.string   "email"
   end
 
   create_table "posts", force: :cascade do |t|
