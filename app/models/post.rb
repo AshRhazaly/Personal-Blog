67571bf60108115ef6f29a106e96a91e83067a7d
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-09 10:00:23 +0800

    Add responsive blog header

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,9 @@ class Post < ApplicationRecord
                     uniqueness: {case_sensitive: false}
   validates :blog_author, presence: true,
                           length: {minimum: 5},
-                          format: {without: /\d/, message: "No numbers bro."}
+                          format: {without: /\d/, message: "No numbers bro."},
+                          uniqueness: {case_sensitive: false}
   validates :content, presence: true,
-                      length: {minimum: 250, maximum: 2000}
+                      length: {minimum: 250, maximum: 2000},
+                      uniqueness: {case_sensitive: false}
 end
