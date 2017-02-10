f0a43ee8f2113d3f43caf8c9308f3666793d5964
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-10 09:38:05 +0800

    Change email format validation to regex

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   validates :name, presence: true
-  validates :email, email_format: { message: "doesn't look like an email address" },
-                    :presence => true
+  validates :email, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :content, presence: true
 end
